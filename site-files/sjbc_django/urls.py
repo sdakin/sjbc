81fe1aa95c8489284a7b0df0a3495b41c1bde0d5
Steve Dakin <sddakin@gmail.com>
2015-10-17 20:42:15 -0700

    updated config settings to support both dev and prod envs

--- site-files/sjbc_django/urls.py
+++ site-files/sjbc_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from api.views import about, store, getInvolved
+from sjbc_django.views import about, store, getInvolved
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
