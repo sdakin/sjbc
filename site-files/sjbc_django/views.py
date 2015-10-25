65e90a0bb4fd71fde65159f63fcce0f3bc3bcbb6
Steve Dakin <sddakin@gmail.com>
2015-10-25 16:32:37 -0700

    added decorator to always send csrf cookie

--- site-files/sjbc_django/views.py
+++ site-files/sjbc_django/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
 from django.template import RequestContext
+from django.views.decorators.csrf import ensure_csrf_cookie
 from store.bicycle import Bicycle
 
 def get_profile_name(request):
@@ -13,18 +14,23 @@ def get_default_context(request):
 
 
 # ---- Top-level navigation view handlers ----
+@ensure_csrf_cookie
 def about(request):
     return render_to_response('about.html', context=get_default_context(request))
 
+@ensure_csrf_cookie
 def store(request):
     return render_to_response('store.html', context=get_default_context(request))
 
+@ensure_csrf_cookie
 def getInvolved(request):
     return render_to_response('getInvolved.html', context=get_default_context(request))
 
+@ensure_csrf_cookie
 def donate(request):
     return render_to_response('donate.html', context=get_default_context(request))
 
+@ensure_csrf_cookie
 def do_admin(request):
     if request.user.is_authenticated() and request.user.is_staff:
         return render_to_response('admin.html', context=get_default_context(request))
